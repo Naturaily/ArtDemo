c7044bfb199b56b8cd91baefc7e59202524aef97
Pawel Cybulski <pawel.cybulski@naturaily.com>
2018-10-26 11:39:51 +0200

    testga

--- static.config.js
+++ static.config.js
@@ -113,8 +113,10 @@ export default {
     <Html lang="en-US">
       <Head>
         <meta charSet="UTF-8" />
+        <title>Title Land</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="description" content="hehe descriptionezz" />
+        <meta name="google-site-verification" content="vDMlfF8AyuuZSYxkC4GcaZFIiSzSq1yHqj5MxsEbWok" />
       </Head>
       <Body>{children}</Body>
     </Html>
