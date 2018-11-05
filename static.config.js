92f2141ea5f29205c54243a0a79a1d871f76704f
Pawel Cybulski <pawel.cybulski@naturaily.com>
2018-11-05 11:22:03 +0100

    conf update

--- static.config.js
+++ static.config.js
@@ -21,8 +21,6 @@ function getPosts () {
             const data = fs.readFileSync(item.path, 'utf8')
             // Convert to frontmatter object and markdown content //
             const dataObj = matter(data)
-            // Create slug for URL //
-            dataObj.data.slug = dataObj.data.title.toLowerCase().replace(/ /g, '-').replace(/[^\w-]+/g, '')
             // Remove unused key //
             delete dataObj.orig
             // Push object into items array //
