b571c813defa0688c95bf040350c762574dc1b0b
Pawel Cybulski <pawel.cybulski@naturaily.com>
2018-11-16 11:24:56 +0100

    update link

--- src/components/SiteHeader.js
+++ src/components/SiteHeader.js
@@ -21,7 +21,7 @@ class SiteHeader extends React.Component {
               </div>
             </div>
             <div className="col ml-auto app__user-panel pr-sm-0">
-              <Link to="/logowanie" className="link--muted">Logowanie</Link>
+              <Link to="https://artinfodemo.naturaily.eu/logowanie" className="link--muted">Logowanie</Link>
             </div>
           </nav>
         </div>
